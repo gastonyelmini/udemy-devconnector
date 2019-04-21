ac25f1a5e794d681da118c8f38509f92450a2b64
Gaston Yelmini <gastonyelmini@gmail.com>
2019-04-20 23:45:46 -0300

    Add login feature with jwt

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 // TODO: move keys to .env
 module.exports = {
   mongoURI:
-    "mongodb+srv://gastonyelmini:38150144g@cluster0-n9gon.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://gastonyelmini:38150144g@cluster0-n9gon.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
 };
